44ffa34566577b941c9ee3049eea58d81444e23b
Iam-DeepakVel <deepak.vel82@gmail.com>
2023-11-30 18:18:26 +0530

    refactor: see more button

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -1,30 +1,70 @@
-import React from 'react'
-import Headings from './layout/Headings'
-import MembersCard from './layout/MembersCard'
-import kavikumar from '../assets/kavikumar.jpg'
+import React from "react";
+import Headings from "./layout/Headings";
+import MembersCard from "./layout/MembersCard";
+import kavikumar from "../assets/kavikumar.jpg";
 import { FaArrowRight } from "react-icons/fa6";
-import Button from './layout/Button';
-import { Link } from 'react-scroll'
+import Button from "./layout/Button";
+import { Link } from "react-scroll";
 
 function Members() {
-    return (
-        <div className='min-h-screen flex-col items-center md:px-32 px-5 my-10'>
-            <Headings title1="Our" title2="Members"></Headings>
-            <div className='flex flex-row gap-6 mt-6 overflow-x-auto'>
-                <MembersCard img={kavikumar} name='kavikumar' job='Software developer' institution='CIT' event='10,Half Marathon' contact='ckavikumar2k@gmail.com' batch='2020-2024' />
-                <MembersCard img={kavikumar} name='kavikumar' job='Software developer' institution='CIT' event='10,Half Marathon' contact='ckavikumar2k@gmail.com' batch='2020-2024' />
-                <MembersCard img={kavikumar} name='kavikumar' job='Software developer' institution='CIT' event='10,Half Marathon' contact='ckavikumar2k@gmail.com' batch='2020-2024' />
-                <MembersCard img={kavikumar} name='kavikumar' job='Software developer' institution='CIT' event='10,Half Marathon' contact='ckavikumar2k@gmail.com' batch='2020-2024' />
-                <MembersCard img={kavikumar} name='kavikumar' job='Software developer' institution='CIT' event='10,Half Marathon' contact='ckavikumar2k@gmail.com' batch='2020-2024' />
-                <div className='flex flex-row items-center space-x-5'>
-                <Link to='' spy={true} smooth={true} duration={500}>
-                    <Button title="SeeMore"></Button>
-                </Link>
-                <FaArrowRight to=" " className='h-10 w-10 cursor-pointer'/>
-                </div>
-            </div>
+  return (
+    <div className="min-h-screen flex-col items-center md:px-32 px-5 my-10">
+      <Headings title1="Our" title2="Members"></Headings>
+      <div className="flex flex-row gap-6 mt-6 overflow-x-auto">
+        <MembersCard
+          img={kavikumar}
+          name="kavikumar"
+          job="Software developer"
+          institution="CIT"
+          event="10,Half Marathon"
+          contact="ckavikumar2k@gmail.com"
+          batch="2020-2024"
+        />
+        <MembersCard
+          img={kavikumar}
+          name="kavikumar"
+          job="Software developer"
+          institution="CIT"
+          event="10,Half Marathon"
+          contact="ckavikumar2k@gmail.com"
+          batch="2020-2024"
+        />
+        <MembersCard
+          img={kavikumar}
+          name="kavikumar"
+          job="Software developer"
+          institution="CIT"
+          event="10,Half Marathon"
+          contact="ckavikumar2k@gmail.com"
+          batch="2020-2024"
+        />
+        <MembersCard
+          img={kavikumar}
+          name="kavikumar"
+          job="Software developer"
+          institution="CIT"
+          event="10,Half Marathon"
+          contact="ckavikumar2k@gmail.com"
+          batch="2020-2024"
+        />
+        <MembersCard
+          img={kavikumar}
+          name="kavikumar"
+          job="Software developer"
+          institution="CIT"
+          event="10,Half Marathon"
+          contact="ckavikumar2k@gmail.com"
+          batch="2020-2024"
+        />
+        <div className="flex flex-row items-center space-x-5">
+          <Link to="" spy={true} smooth={true} duration={500} className="relative">
+            <Button title="SeeMore"></Button>
+            <FaArrowRight size={16} className="cursor-pointer absolute top-[30px] right-2" />
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Members
+export default Members;
--- src/Components/layout/Button.js
+++ src/Components/layout/Button.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = (props) => {
   return (
     <div>
-      <button className='bg-white py-2 px-5 rounded-full mt-4 outline hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:bg-yellow-300 hover:text-black transition-all'>
+      <button className='bg-white py-2 pr-8 pl-5 rounded-full mt-4 outline hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:bg-yellow-300 hover:text-black transition-all'>
         {props.title}
       </button>
     </div>
