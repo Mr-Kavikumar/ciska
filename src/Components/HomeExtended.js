24d133f24574f83ab746d1d745b6fb6ee5d2ed5f
Mr-Kavikumar <ckavikumar2k@gmail.com>
2023-12-01 17:40:54 +0530

    Started Working on the Extension of the Home Page

--- src/Components/HomeExtended.js
+++ src/Components/HomeExtended.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Link } from 'react-scroll'
-import Button from './layout/Button'
+import NavBar from './NavBar'
 import logo from '../assets/Logo.png'
-import HomeExtended from './HomeExtended'
 
-function Home() {
-    return (
-        <div className='min-h-[70vh] flex flex-col md:flex-row md:justify-between items-center md:mx-32 mx-5 mt-10 '>
+function HomeExtended() {
+  return (
+    <div>
+      <NavBar/>
+      <main>
+      <div className='min-h-[70vh] flex flex-col md:flex-row md:justify-between items-center md:mx-32 mx-5 mt-10 '>
             <div className='md:w-2/4 text-center'>
                 <h2 className='text-5xl font-semibold leading-tight'>CISKA</h2>
                 <div className='flex justify-center pt-5 pb-5'>
                     <img src={logo} className='h-40 w-40 cursor-pointer flex justify-center md:hidden'></img>
                 </div>
-
                 <p className='text-gray-800 mt-5 text-start'>
                     CISKA youth social trust was founded by Dr.M Veeraseenu
                     in 2008 for the well being of students who are unable to
@@ -21,15 +21,14 @@ function Home() {
                     the members of this trusts includes the students of <a href='https://www.cit.edu.in/' className='text-blue-800'>COIMBATORE INSTITUTE OF TECHNOLOGY</a>,
                     JANSON ENGINEEERING COLLEGE AND TAMILNADU ENGINEERING COLLEGE
                 </p>
-                <Link to={HomeExtended} spy={true} smooth={true} duration={500}>
-                    <Button title="see more" />
-                </Link>
             </div>
             <div className='w-full md:w-2/4 hidden md:block'>
                 <img src={logo} className='cursor-pointer flex justify-center'></img>
             </div>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
 
-export default Home
+export default HomeExtended
