30bac2b2fc856ac560be6d15850b137239d1b2eb
Mr-Kavikumar <ckavikumar2k@gmail.com>
2023-12-08 12:58:38 +0530

    Working on WorksCard

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -15,8 +15,6 @@ function Members() {
     <div className="min-h-screen flex-col items-center md:px-32 px-5 my-10">
       <Headings title1="Our" title2="Members"></Headings>
       <div className="flex flex-row gap-6 mt-6 overflow-x-auto">
-      <Splide aria-aria-label="Member card">
-        <SplideSlide>
         <MembersCard
           img={kavikumar}
           name="kavikumar"
@@ -26,8 +24,7 @@ function Members() {
           contact="ckavikumar2k@gmail.com"
           batch="2020-2024"
         />
-        </SplideSlide>
-        <SplideSlide>
+    
         <MembersCard
           img={Ganesh}
           name="GaneshMoorthy"
@@ -37,8 +34,7 @@ function Members() {
           contact="ckavikumar2k@gmail.com"
           batch="2021-2025"
         />
-        </SplideSlide>
-        <SplideSlide>
+     
         <MembersCard
           img={kavikumar}
           name="kavikumar"
@@ -48,8 +44,7 @@ function Members() {
           contact="ckavikumar2k@gmail.com"
           batch="2020-2024"
         />
-        </SplideSlide>
-        <SplideSlide>
+        
         <MembersCard
           img={Ganesh}
           name="GaneshMoorthy"
@@ -59,8 +54,7 @@ function Members() {
           contact="ckavikumar2k@gmail.com"
           batch="2021-2025"
         />
-        </SplideSlide>
-        <SplideSlide>
+      
         <MembersCard
           img={kavikumar}
           name="kavikumar"
@@ -70,8 +64,7 @@ function Members() {
           contact="ckavikumar2k@gmail.com"
           batch="2020-2024"
         />
-        </SplideSlide>
-        <SplideSlide>
+     
         <MembersCard
           img={Ganesh}
           name="GaneshMoorthy"
@@ -81,16 +74,13 @@ function Members() {
           contact="ckavikumar2k@gmail.com"
           batch="2021-2025"
         />
-        </SplideSlide>
-        <SplideSlide>
+        
         <div className="flex flex-row items-center space-x-5 pr-5">
           <Link to="" spy={true} smooth={true} duration={500} className="relative">
             <Button title="SeeMore"></Button>
             <FaArrowRight size={16} className="cursor-pointer absolute top-[30px] right-2" />
           </Link>
         </div>
-        </SplideSlide>
-      </Splide>
       </div>
     </div>
   );
--- src/Components/layout/WorksCard.js
+++ src/Components/layout/WorksCard.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-function WorksCard({ image, title , para}) {
+function WorksCard(props) {
     return (
         <div className='group flex flex-col items-center text-center gap-2 w-full lg:w-1/3 p-5 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] 
         rounded-lg cursor-pointer lg:hover:-translate-y-6 transition duration-300 ease-in-out'>
             <div>
-                <img className='h-40 w-full' src={image}>
+                <img className='h-40 w-full' src={props.image}>
                 </img>
             </div>
             <h1 className='font-semibold text-lg'>
-                {title}
+                {props.title}
             </h1>
             <p>
-                {para}
+                {props.para}
             </p>
             <h3 className='text-gray-400 cursor-pointer hover:text-yellow-300 transition duration-300 ease-in-out'>
                 Explore More
