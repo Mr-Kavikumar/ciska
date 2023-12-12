979d804b5328c030bb368ed2035a1fbdc8c9bb17
Mr-Kavikumar <ckavikumar2k@gmail.com>
2023-12-12 14:54:39 +0530

    Working on WorksCard

--- src/Components/Members.js
+++ src/Components/Members.js
@@ -6,15 +6,13 @@ import { FaArrowRight } from "react-icons/fa6";
 import Button from "./layout/Button";
 import { Link } from "react-scroll";
 import Ganesh from "../assets/Ganesh.jpg"
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
 
 
 function Members() {
   return (
     <div className="min-h-screen flex-col items-center md:px-32 px-5 my-10">
       <Headings title1="Our" title2="Members"></Headings>
-      <div className="flex flex-row gap-6 mt-6 overflow-x-auto">
+      <div className="flex flex-col items-center md:flex md:flex-row gap-6 mt-6 overflow-y-auto">
         <MembersCard
           img={kavikumar}
           name="kavikumar"
@@ -75,7 +73,7 @@ function Members() {
           batch="2021-2025"
         />
         
-        <div className="flex flex-row items-center space-x-5 pr-5">
+        <div className="flex flex-row items-center space-x-5 pr-5 pb-4">
           <Link to="" spy={true} smooth={true} duration={500} className="relative">
             <Button title="SeeMore"></Button>
             <FaArrowRight size={16} className="cursor-pointer absolute top-[30px] right-2" />
