2232ad19d53165cc0d01c0c3df916e1ad2f6de8e
Guna-Pandi <gunapandidurai@gmail.com>
2024-02-15 16:11:49 +0530

    Feat: Gallery1 Updated

--- src/Components/AllGallery/Gallery1.js
+++ src/Components/AllGallery/Gallery1.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { MdClose } from "react-icons/md";
-import Headings from "../layout/Headings";
 
 const Gallery1 = () => {
   const images = [
@@ -37,76 +36,95 @@ const Gallery1 = () => {
 
   return (
     <div>
-    <div className="flex flex-col items-center lg:flex-row justify-center pb-4 m-2">
-        <Headings title1="Event" title2=" Name" />
+      <div className="flex flex-col items-center lg:flex-row justify-center pb-4 m-2">
+      <h1 className="text-6xl tracking-widest">Name</h1>
       </div>
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 m-3">
-      <div className="grid gap-4">
-        {col1.map((image, index) => (
-          <div key={index} onClick={() => openModal(image)}>
-            <img
-              className="h-auto max-w-full rounded-lg cursor-pointer"
-              src={image}
-              alt=""
-            />
+      <div className="flex flex-col lg:flex-row ">
+        <div className="max-w-5xl m-4 lg:w-1/2 lg:pr-4">
+          <img
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+            alt="Cit College"
+            className="mb-6 shadow-lg rounded-lg shadow-white w-full h-96"
+          />
+        </div>
+
+        <div className="max-w-5xl m-4 lg:w-1/2">
+          <div className="mb-6">
+            <h1 className="text-5xl tracking-widest">Name</h1>
           </div>
-        ))}
-      </div>
-      <div className="grid gap-4">
-        {col2.map((image, index) => (
-          <div key={index} onClick={() => openModal(image)}>
-            <img
-              className="h-auto max-w-full rounded-lg cursor-pointer"
-              src={image}
-              alt=""
-            />
-          </div>
-        ))}
-      </div>
-      <div className="grid gap-4">
-        {col3.map((image, index) => (
-          <div key={index} onClick={() => openModal(image)}>
-            <img
-              className="h-auto max-w-full rounded-lg cursor-pointer"
-              src={image}
-              alt=""
-            />
-          </div>
-        ))}
-      </div>
-      <div className="grid gap-4">
-        {col4.map((image, index) => (
-          <div key={index} onClick={() => openModal(image)}>
-            <img
-              className="h-auto max-w-full rounded-lg cursor-pointer"
-              src={image}
-              alt=""
-            />
-          </div>
-        ))}
+          <p className="mb-6 text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque reprehenderit quas, delectus rem deserunt accusamus temporibus, fugiat mollitia provident beatae totam necessitatibus, quos tenetur. Doloremque, a voluptates beatae sed accusamus explicabo voluptatem quae delectus, excepturi enim vitae ratione cupiditate, ab nam sit aperiam? Alias praesentium, excepturi maxime omnis amet, fugiat quas ratione voluptatibus libero optio vitae ex facere vel saepe ad impedit molestias commodi voluptas! Ea minima velit autem quidem?
+          </p>
+        </div>
       </div>
 
-      {selectedImage && (
-        <div
-          className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75"
-          onClick={closeModal} 
-        >
-          <div className="relative bg-white p-4 rounded-lg">
-            <img
-              className="max-w-full max-h-full rounded-lg"
-              src={selectedImage}
-              alt=""
-            />
-            <button
-              className="absolute top-2 right-2 text-gray-700 cursor-pointer"
-              onClick={closeModal}
-            >
-              <MdClose size={24} />
-            </button>
-          </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 m-3">
+        <div className="grid gap-4">
+          {col1.map((image, index) => (
+            <div key={index} onClick={() => openModal(image)}>
+              <img
+                className="h-auto max-w-full rounded-lg cursor-pointer"
+                src={image}
+                alt=""
+              />
+            </div>
+          ))}
         </div>
-      )}
-    </div>
+        <div className="grid gap-4">
+          {col2.map((image, index) => (
+            <div key={index} onClick={() => openModal(image)}>
+              <img
+                className="h-auto max-w-full rounded-lg cursor-pointer"
+                src={image}
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
+        <div className="grid gap-4">
+          {col3.map((image, index) => (
+            <div key={index} onClick={() => openModal(image)}>
+              <img
+                className="h-auto max-w-full rounded-lg cursor-pointer"
+                src={image}
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
+        <div className="grid gap-4">
+          {col4.map((image, index) => (
+            <div key={index} onClick={() => openModal(image)}>
+              <img
+                className="h-auto max-w-full rounded-lg cursor-pointer"
+                src={image}
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
+
+        {selectedImage && (
+          <div
+            className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75"
+            onClick={closeModal}
+          >
+            <div className="relative bg-white p-4 rounded-lg">
+              <img
+                className="max-w-full max-h-full rounded-lg"
+                src={selectedImage}
+                alt=""
+              />
+              <button
+                className="absolute top-2 right-2 text-gray-700 cursor-pointer"
+                onClick={closeModal}
+              >
+                <MdClose size={24} />
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
