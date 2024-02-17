ce48a12aeb77b6ac44988907fe7d94f2192b0a22
Guna-Pandi <gunapandidurai@gmail.com>
2024-02-17 18:23:03 +0530

    Cleared Warnings

--- src/Components/HomeExtended.js
+++ src/Components/HomeExtended.js
@@ -10,7 +10,7 @@ function HomeExtended() {
         <div className='md:w-2/4 text-center'>
           <h2 className='text-5xl font-semibold leading-tight'>CISKA</h2>
           <div className='flex justify-center pt-5 pb-5'>
-            <img src={logo} className='h-40 w-40 cursor-pointer flex justify-center md:hidden'></img>
+            <img src={logo} className='h-40 w-40 cursor-pointer flex justify-center md:hidden' alt=''/>
           </div>
           <p className='text-gray-800 mt-5 text-start'>
             CISKA youth social trust was founded by Dr.M Veeraseenu
@@ -22,14 +22,14 @@ function HomeExtended() {
           </p>
         </div>
         <div className='w-full md:w-2/6 hidden md:block'>
-          <img src={logo} className='cursor-pointer flex justify-center'></img>
+          <img src={logo} className='cursor-pointer flex justify-center'alt=''/>
         </div>
       </div>
       <div className='min-h-[70vh] flex flex-col md:flex-row-reverse  md:justify-between items-center md:mx-32 mx-5 mt-10 '>
         <div className='md:w-2/4 text-center'>
           <h2 className='text-5xl font-semibold leading-tight'>Founder Of CISKA</h2>
           <div className='flex justify-center pt-5 pb-5'>
-            <img src={Coach} className='h-50 w-40 cursor-pointer flex justify-center rounded-lg md:hidden'></img>
+            <img src={Coach} className='h-50 w-40 cursor-pointer flex justify-center rounded-lg md:hidden'alt=''/>
           </div>
           <p className='text-gray-800 mt-5 text-start'>
             CISKA youth social trust was founded by Dr.M Veeraseenu
@@ -41,14 +41,14 @@ function HomeExtended() {
           </p>
         </div>
         <div className='w-full md:w-2/6 hidden md:block'>
-          <img src={Coach} className='cursor-pointer flex justify-center rounded-lg'></img>
+          <img src={Coach} className='cursor-pointer flex justify-center rounded-lg'alt=''/>
         </div>
       </div>
       <div className='min-h-[70vh] flex flex-col md:flex-row md:justify-between items-center md:mx-32 mx-5 mt-10 '>
         <div className='md:w-2/4 text-center'>
           <h2 className='text-5xl font-semibold leading-tight'>Treasurer</h2>
           <div className='flex justify-center pt-5 pb-5'>
-            <img src={logo} className='h-40 w-40 cursor-pointer flex justify-center md:hidden'></img>
+            <img src={logo} className='h-40 w-40 cursor-pointer flex justify-center md:hidden'alt=''/>
           </div>
           <p className='text-gray-800 mt-5 text-start'>
             CISKA youth social trust was founded by Dr.M Veeraseenu
@@ -60,7 +60,7 @@ function HomeExtended() {
           </p>
         </div>
         <div className='w-full md:w-2/6 hidden md:block'>
-          <img src={logo} className='cursor-pointer flex justify-center'></img>
+          <img src={logo} className='cursor-pointer flex justify-center'alt=''/>
         </div>
       </div>
     </div>
